668fd851aa18c798e6ed079da08d1ea7599a9c59
Leonald <leufrasiojunior@gmail.com>
2024-03-31 21:00:08 -0300

    Reestruturação

--- src/context/AxiosInstance/index.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const connectEvolution = axios.create({
-    baseURL: 'http://localhost:8080',
-    headers: {
-        Accept: 'application/json',
-        apikey:'B6D711FCDE4D4FD5936544120E713976'
-    },
-  });
